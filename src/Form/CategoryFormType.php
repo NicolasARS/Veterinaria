51f9fbbeff050a9af119eb18a6bd24b2e17d27dc
NicolasARS <aluriv9412@ieselcaminas.org>
2023-11-10 10:22:04 +0100

    Aplicacion terminada, zona administracion terminada

--- src/Form/CategoryFormType.php
+++ src/Form/CategoryFormType.php
@@ -2,32 +2,27 @@
 
 namespace App\Form;
 
-use App\Entity\Contact;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContactFormType extends AbstractType
+class CategoryFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name', TextType::class)
-            ->add('phoneNumber', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('message', TextareaType::class)
-            ->add('Send', SubmitType::class, array('label' => 'Send Message'));
+            ->add('Send', SubmitType::class, array('label' => 'Create new category'));
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Contact::class,
+            'data_class' => Category::class,
         ]);
     }
 }
